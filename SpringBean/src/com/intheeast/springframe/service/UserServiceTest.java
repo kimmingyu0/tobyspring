c2b24a42ed93278f75c70edd6c7cfb811f31a1e7
kimmingyu0 <minku4820@gmail.com>
2023-07-11 14:10:06 +0900

    SpringBean practice init

---
+++ SpringBean/src/com/intheeast/springframe/service/UserServiceTest.java
@@ -0,0 +1,235 @@
+package com.intheeast.springframe.service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.PlatformTransactionManager;
+
+//import org.springframework.mail.MailSender;
+
+import com.intheeast.springframe.dao.UserDao;
+import com.intheeast.springframe.domain.Level;
+import com.intheeast.springframe.domain.User;
+
+import static com.intheeast.springframe.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
+import static com.intheeast.springframe.service.UserService.MIN_RECCOMEND_FOR_GOLD;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestServiceFactory.class})
+public class UserServiceTest {
+	@Autowired 	UserService userService;	
+	@Autowired UserDao userDao;	
+	@Autowired MailSender mailSender; 
+	@Autowired PlatformTransactionManager transactionManager;
+	
+	List<User> users;	// test fixture
+	
+	@BeforeEach
+	public void setUp() {	
+		
+		users = Arrays.asList(
+				new User("bumjin", "박범진", "p1", "user1@ksug.org", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER-1, 0),
+				new User("joytouch", "강명성", "p2", "user2@ksug.org", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER, 0),
+				new User("erwins", "신승한", "p3", "user3@ksug.org", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD-1),
+				new User("madnite1", "이상호", "p4", "user4@ksug.org", Level.SILVER, 60, MIN_RECCOMEND_FOR_GOLD),
+				new User("green", "오민규", "p5", "user5@ksug.org", Level.GOLD, 100, Integer.MAX_VALUE)
+				);
+	}
+	
+	@Test @DirtiesContext
+	public void upgradeLevels() throws Exception {
+		userDao.deleteAll();
+		for(User user : users) userDao.add(user);
+		
+		MockMailSender mockMailSender = new MockMailSender();
+		userService.setMailSender(mockMailSender);  
+				
+		userService.upgradeLevels();
+		
+		checkLevelUpgraded(users.get(0), false);
+		checkLevelUpgraded(users.get(1), true);
+		checkLevelUpgraded(users.get(2), false);
+		checkLevelUpgraded(users.get(3), true);
+		checkLevelUpgraded(users.get(4), false);
+		
+		List<String> request = mockMailSender.getRequests();  
+		assertEquals(request.size(), 2);
+		assertEquals(request.get(0), users.get(1).getEmail());
+		assertEquals(request.get(1), users.get(3).getEmail());		
+	}
+	
+	@Test
+	public void sendEmailToGmail() throws UnsupportedEncodingException {
+		//JavaMailSenderImpl sender = new JavaMailSenderImpl();
+		SimpleMailMessage emailMessage = new SimpleMailMessage();
+		
+		String host = "smtp.gmail.com";
+        int port = 587; // TLS : 587, SSL : 465
+        String username = "swseokitec@gmail.com";  // 발신자 Gmail 계정
+        String password = "kmwmvsbajccozsxc";  // 발신자 Gmail 계정 비밀번호
+
+        // 수신자 이메일 주소
+        String toAddress = "intheeast0305@gmail.com";
+
+        // 메일 속성 설정
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+
+        // 인증 객체 생성
+        Authenticator authenticator = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        };
+
+        // 세션 생성
+        Session session = Session.getInstance(props, authenticator);
+
+        try {
+            // MimeMessage 생성
+        	MimeMessage message = new MimeMessage(session);
+            
+//            MimeMessageHelper mailHelper = new MimeMessageHelper(message, true, "UTF-8");
+//            
+//            mailHelper.setFrom(from);
+//            mailHelper.setTo(to);
+//            mailHelper.setSubject(subject);
+//            mailHelper.setText(content, true);
+            
+        	message.setFrom(new InternetAddress(username));
+        	message.setRecipient(Message.RecipientType.TO, new InternetAddress(toAddress));
+            message.setSubject(MimeUtility.encodeText("반가워요", "UTF-8", "B"));
+            message.setText("테스트 메일입니다.", "UTF-8");
+
+            // 메일 전송
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+        } catch (MessagingException e) {
+            System.out.println("Failed to send email. Error message: " + e.getMessage());
+            fail("This sendEmailToGmail test is failed!!!");
+        }
+    }	
+	
+	static class MockMailSender implements MailSender {
+		private List<String> requests = new ArrayList<String>();	
+		
+		public List<String> getRequests() {
+			return requests;
+		}
+
+		public void send(SimpleMailMessage mailMessage) /*throws MailException*/ {
+			requests.add(mailMessage.getTo()[0]);  
+		}
+
+		public void send(SimpleMailMessage[] mailMessage) throws MailException {
+		}
+	}
+
+	private void checkLevelUpgraded(User user, boolean upgraded) {
+		Optional<User> optionalUser = userDao.get(user.getId());
+		if (!optionalUser.isEmpty()) {
+			User userUpdate = optionalUser.get();
+			if (upgraded) {
+				assertEquals(userUpdate.getLevel(), user.getLevel().nextLevel());
+			}
+			else {
+				assertEquals(userUpdate.getLevel(), (user.getLevel()));
+			}			
+		}		
+	}
+	
+	@Test
+	public void add() {
+		userDao.deleteAll();
+		
+		User userWithLevel = users.get(4);	  // GOLD ����  
+		User userWithoutLevel = users.get(0);  
+		userWithoutLevel.setLevel(null);
+		
+		userService.add(userWithLevel);	  
+		userService.add(userWithoutLevel);
+		
+		Optional<User> optionalUserWithLevelRead = userDao.get(userWithLevel.getId());
+		if (!optionalUserWithLevelRead.isEmpty()) {
+			User userWithLevelRead = optionalUserWithLevelRead.get();
+			assertEquals(userWithLevelRead.getLevel(), userWithLevel.getLevel()); 
+		}		
+		
+		Optional<User> optionalUserWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+		if (!optionalUserWithoutLevelRead.isEmpty()) {
+			User userWithoutLevelRead = optionalUserWithoutLevelRead.get();
+			assertEquals(userWithoutLevelRead.getLevel(), Level.BASIC);
+		}		
+	}
+	
+	@Test
+	public void upgradeAllOrNothing() throws Exception {
+		UserService testUserService = new TestUserService(users.get(3).getId());  
+		testUserService.setUserDao(this.userDao); 
+		testUserService.setTransactionManager(this.transactionManager);
+		testUserService.setMailSender(this.mailSender);
+		
+		userDao.deleteAll();			  
+		for(User user : users) userDao.add(user);
+		
+		try {
+			testUserService.upgradeLevels();   
+			fail("TestUserServiceException expected"); 
+		}
+		catch(TestUserServiceException e) { 
+		}
+		
+		checkLevelUpgraded(users.get(1), false);
+	}
+	
+	static class TestUserService extends UserService {
+		private String id;
+		
+		private TestUserService(String id) {  
+			this.id = id;
+		}
+
+		protected void upgradeLevel(User user) {
+			if (user.getId().equals(this.id)) throw new TestUserServiceException();  
+			super.upgradeLevel(user);  
+		}
+	}
+	
+	static class TestUserServiceException extends RuntimeException {
+	}
+
+}
